67b69a5299654e0ebe56bce265461db9d9779d2e
JordanG <jmg236@yahoo.com>
2019-06-14 12:00:46 -0500

    server

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const helmet = require("helmet");
+
+// const actionRouter = require("../action/action-router.js");
+// const projectRouter = require("../project/project-router.js");
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+// server.use("/api/action", actionRouter);
+// server.use("/api/project", projectRouter);
+
+
+module.exports = server;
